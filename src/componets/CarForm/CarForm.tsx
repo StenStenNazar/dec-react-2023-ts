e476285a53581231a34cb14018f5216ad7417b0d
StenStenNazar <naziksten@gmail.com>
2023-04-20 16:38:43 +0300

    hw3-ts

--- src/componets/CarForm/CarForm.tsx
+++ src/componets/CarForm/CarForm.tsx
@@ -1,8 +1,12 @@
 import {FC, useEffect} from 'react'
-import {ICar} from "../../interfaces/car.interface";
+import {joiResolver} from "@hookform/resolvers/joi";
+
 import {SubmitHandler, useForm} from "react-hook-form";
 import {carService} from "../../services/car.service";
 import {IUseState} from "../../types/useStateType";
+import {ICar} from "../../interfaces/car.interface";
+import {carValidator} from "../../validators/car.validator";
+import './CarForm.css'
 
 interface IProps {
     setCars: IUseState<ICar[]>
@@ -16,7 +20,13 @@ const CarForm: FC<IProps> = ({
                                  setState, setCarForUpdate
                              }) => {
 
-    const {register, reset, handleSubmit, setValue, formState: {errors, isValid}} = useForm<ICar>();
+    const {
+        register,
+        reset,
+        handleSubmit,
+        setValue,
+        formState: {errors, isValid}
+    } = useForm<ICar>({mode: 'all', resolver: joiResolver(carValidator)});
 
     useEffect(() => {
         if (carForUpdate) {
@@ -47,12 +57,25 @@ const CarForm: FC<IProps> = ({
 
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(carForUpdate ? update : save)}>
-                <input type="text" placeholder={'brand'} {...register('brand')}/>
-                <input type="number" placeholder={'year'} {...register('year')}/>
-                <input type="number" placeholder={'price'} {...register('price')}/>
-                <button disabled={!isValid}>{carForUpdate ? 'update' : 'save'}</button>
+        <div className={'car_container'}>
+            <form className={'car_form_teg'} onSubmit={handleSubmit(carForUpdate ? update : save)}>
+
+                <div className={'input_car_form'}>
+                    <input type="text" placeholder={'brand'} {...register('brand')}/>
+                    <div className={'errors'}>{errors.brand && <div>{errors.brand.message}</div>}</div>
+                </div>
+
+                <div className={'input_car_form'}>
+                    <input type="number" placeholder={'price'} {...register('price')}/>
+                    <div className={'errors'}>{errors.price && <div>{errors.price.message}</div>}</div>
+                </div>
+
+                <div className={'input_car_form'}>
+                    <input type="number" placeholder={'year'} {...register('year')}/>
+                    <div className={'errors'}>{errors.year && <div>{errors.year.message}</div>}</div>
+                </div>
+
+                <button className={'delete_update'} disabled={!isValid}>{carForUpdate ? 'update' : 'save'}</button>
             </form>
         </div>
     );
---
+++ src/validators/car.validator.tsx
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const carValidator = Joi.object({
+    brand: Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+        'string.pattern.base': 'only letters min 1 max 20'
+    }),
+    price: Joi.number().min(0).max(1000000).required(),
+    year: Joi.number().min(1990).max(new Date().getFullYear()).required()
+})
